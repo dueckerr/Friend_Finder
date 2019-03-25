b5026ada9b71e3d0f09f39980ac8c3061bba6493
Reagan D Duecker <dueckerr@gmail.com>
2019-03-24 21:27:45 -0600

    1

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+
+var friends = [
+	{
+		name: 'Black Widow',
+		photo: 'https://ksassets.timeincuk.net/wp/uploads/sites/55/2018/02/Black-Widow-Avengers-920x584.jpg',
+		scores: [5, 1, 4, 3, 2, 5, 1, 2, 5, 1 ]
+	},
+	{
+		name: "Thor",
+		photo: 'https://i.pinimg.com/originals/00/6c/05/006c0503226b49354d3870a05709943c.gif',
+		scores: [3, 5, 5, 1, 5, 2, 5, 4, 5, 4 ]
+	},
+	{
+		name: 'Captain America',
+		photo: 'https://www.shrinktank.com/wp-content/uploads/2016/10/3514028-captain-america-the-first-avenger-4fe203cd661b71.jpg',
+		scores: [1, 1, 5, 4, 4, 1, 1, 5, 1, 4 ]
+	},
+	{
+		name: "The Hulk",
+		photo: 'https://media.wired.com/photos/59b83283b412f33ba9daa3f5/191:100/pass/thehulk-FA.jpg',
+		scores: [5, 1, 3, 3, 5, 5, 1, 2, 3, 1 ]
+	},
+	{
+		name: "Iron Man",
+        photo: 'https://www.thewrap.com/wp-content/uploads/2018/04/ironman.jpg',
+        scores: [2, 5, 5, 5, 3, 4, 4, 2, 2, 5]
+	}
+];
+
+module.exports = friends;
